12620a73cbc46a3c205fcb9a5db434708c526175
Abhinav Bhatera <hyphen178@gmail.com>
2023-06-02 19:27:49 +0530

    Division

---
+++ diff.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    float x,y;
+    cout<<"Enter two numbers : ";
+    cin>>x>>y;
+    cout<<"Division of two numbers is : "<<x/y;
+    return 0;
+}
